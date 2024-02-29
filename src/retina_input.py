88b6ee83d88cc41351fbc5c2eb830f4638f20ece
Roger Larsson <roger.larsson@slentelit.se>
2024-02-29 10:29:06 +0100

    With objects

--- src/retina_input.py
+++ src/retina_input.py
@@ -1,6 +1,6 @@
 import dv_processing as dv
 from datetime import timedelta
-from src.retina_process import init_callback, slicing_callback
+from src.retina_process import SpikingNeurons
 
 # Input store:
 reader = dv.io.MonoCameraRecording("../data/dvSave-2024_02_22_15_26_03.aedat4") # cameraSerial)
@@ -8,8 +8,10 @@ reader = dv.io.MonoCameraRecording("../data/dvSave-2024_02_22_15_26_03.aedat4") 
 # Output store: Initialize a slicer
 slicer = dv.EventStreamSlicer()
 
+layer1 = SpikingNeurons()
+
 # Register a callback every 33 milliseconds
-slicer.doEveryTimeInterval(timedelta(milliseconds=33), slicing_callback)
+slicer.doEveryTimeInterval(timedelta(milliseconds=33), layer1.slicing_callback)
 
 # Find start time
 while reader.isRunning():
@@ -20,10 +22,10 @@ while reader.isRunning():
     if events is not None:
         # If so, pass the events into the slicer to handle them
         first_timestamp = events[0].timestamp()
+        layer1.init_callback(first_timestamp)
         break
 
-init_callback(first_timestamp)
-
+# Process
 while reader.isRunning():
     # Receive events
     events = reader.getNextEventBatch()
--- src/retina_process.py
+++ src/retina_process.py
@@ -9,78 +9,78 @@ DISCHARGE = 0.99
 THRESHOLD = 5
 MAX_DT = 1000  # Timestamp ticks
 
-# Open any camera
-neurons = np.zeros(shape=(346, 260))
+class SpikingNeurons:
+    def __init__(self):
+        # Open any camera
+        self._neurons = np.zeros(shape=(346, 260))
 
-# Initialize neurons with something visible
-neurons[:,25] = EVENT_SPIKE
-neurons[25,:] = EVENT_SPIKE
+        # Initialize neurons with something visible
+        self._neurons[:,25] = EVENT_SPIKE
+        self._neurons[25,:] = EVENT_SPIKE
 
-spike_store = dv.EventStore()
+        self._spike_store = dv.EventStore()
 
-slicer = dv.EventStreamSlicer()
+        self._slicer = dv.EventStreamSlicer()
 
 
-#cv.namedWindow("Preview", cv.WINDOW_NORMAL)
-#cv.imshow("Preview", neurons.T)
-fig, ax = plt.subplots(1, 1)
+        #cv.namedWindow("Preview", cv.WINDOW_NORMAL)
+        #cv.imshow("Preview", neurons.T)
+        self._fig, self._ax = plt.subplots(1, 1)
 
-def discharge(prev_ts, next_ts, neuron_layer):
-    dt = 1e-6 * (next_ts - prev_ts)
-    neuron_layer -= dt * DISCHARGE * neuron_layer
-    return next_ts
+        # Run the event processing while the camera is connected
+        self._plot_timestamp = 0
+        self._prev_timestamp = dv.now()
 
-# Declare the callback method for slicer
-def slicing_callback(events: dv.EventStore):
-    global next_timestamp, prev_timestamp, plot_timestamp, spike_store
-    # Pass events into the accumulator and generate a preview frame
-    for event in events:
-        # Inputs
-        e_x = event.x()
-        e_y = event.y()
-        e_p = event.polarity()
-        # Ignore bad pixels
-        if e_x == 231 and e_y == 202:
-            continue
-        neurons[e_x, e_y] += EVENT_SPIKE
-        # Self over threshold?
-        #            if neurons[e_x, e_y] > THRESHOLD:
-        #                print(event.timestamp(), e_x, e_y)
-        next_timestamp = event.timestamp()
-        if next_timestamp - prev_timestamp > MAX_DT:
-            spiking = np.where(neurons > THRESHOLD)
-            neurons[spiking] = 0
-            for x, y in zip(spiking[0], spiking[1]):
-                spike_store.push_back(next_timestamp, x, y, True)
-            slicer.accept(spike_store)
-            prev_timestamp = discharge(prev_timestamp, next_timestamp, neurons)
+    def init_callback(self, first_timestamp):
+        self._prev_timestamp = first_timestamp
 
-    spiking = np.where(neurons > THRESHOLD)
-    neurons[spiking] = 0
-    for x, y in zip(spiking[0], spiking[1]):
-        spike_store.push_back(next_timestamp, x, y, True)
-    slicer.accept(spike_store)
-    #        if len(spiking[0]) or len(spiking[1]):
-    #            print(prev_timestamp, spiking)
+    @classmethod
+    def discharge(cls, prev_ts, next_ts, neuron_layer):
+        dt = 1e-6 * (next_ts - prev_ts)
+        neuron_layer -= dt * DISCHARGE * neuron_layer
+        return next_ts
 
-    # Discharge
-    prev_timestamp = discharge(prev_timestamp, next_timestamp, neurons)
+    # Declare the callback method for slicer
+    def slicing_callback(self, events: dv.EventStore):
+        # Pass events into the accumulator and generate a preview frame
+        for event in events:
+            # Inputs
+            e_x = event.x()
+            e_y = event.y()
+            e_p = event.polarity()
+            # Ignore bad pixels
+            if e_x == 231 and e_y == 202:
+                continue
+            self._neurons[e_x, e_y] += EVENT_SPIKE
+            # Self over threshold?
+            #            if neurons[e_x, e_y] > THRESHOLD:
+            #                print(event.timestamp(), e_x, e_y)
+            next_timestamp = event.timestamp()
+            if next_timestamp - self._prev_timestamp > MAX_DT:
+                spiking = np.where(self._neurons > THRESHOLD)
+                self._neurons[spiking] = 0
+                for x, y in zip(spiking[0], spiking[1]):
+                    self._spike_store.push_back(next_timestamp, x, y, True)
+                self._slicer.accept(self._spike_store)
+                self._prev_timestamp = SpikingNeurons.discharge(self._prev_timestamp, next_timestamp, self._neurons)
 
-    # Display
-    if next_timestamp - plot_timestamp > 1e6:
-        # Generate and show a preview of recent tracking history
-        # cv.imshow("Preview", neurons.T)
-        plt.imshow(neurons.T)
-        plt.show()
+        spiking = np.where(self._neurons > THRESHOLD)
+        self._neurons[spiking] = 0
+        for x, y in zip(spiking[0], spiking[1]):
+            self._spike_store.push_back(next_timestamp, x, y, True)
+        self._slicer.accept(self._spike_store)
+        #        if len(spiking[0]) or len(spiking[1]):
+        #            print(prev_timestamp, spiking)
 
-        plot_timestamp = next_timestamp
-        print(f"\r{plot_timestamp}   ", end='')
+        # Discharge
+        self._prev_timestamp = SpikingNeurons.discharge(self._prev_timestamp, next_timestamp, self._neurons)
 
-# Run the event processing while the camera is connected
-plot_timestamp = 0
-prev_timestamp = next_timestamp = dv.now()
+        # Display
+        if next_timestamp - self._plot_timestamp > 1e6:
+            # Generate and show a preview of recent tracking history
+            # cv.imshow("Preview", self._neurons.T)
+            plt.imshow(self._neurons.T)
+            plt.show()
 
-def init_callback(first_timestamp):
-    global prev_timestamp, next_timestamp
-
-    prev_timestamp = next_timestamp = first_timestamp
+            self._plot_timestamp = next_timestamp
+            print(f"\r{self._plot_timestamp}   ", end='')
